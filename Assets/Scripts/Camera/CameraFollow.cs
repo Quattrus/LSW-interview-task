d60c5091ddef97b7ae1b0e487594b3e83d421915
Romeo IV <94739286+Quattrus@users.noreply.github.com>
2022-11-27 16:48:37 +0800

    Added Extra Art assets and camera follow script
    Added extra art assets as well as added a camera follow script that allows the camera to follow the player's position.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    private Vector3 cameraOffset = new Vector3(0, 0, -15f);
+
+
+    private void Update()
+    {
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
